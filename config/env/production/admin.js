2ea852bd7dad86c711f827e94cf46d3ea0df3def
Diego Albitres <diego.albitres@gmail.com>
2022-09-19 00:31:51 -0500

    chore: setup production env config

---
+++ config/env/production/admin.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  watchIgnoreFiles: [
+    '**/config/sync/**',
+  ],
+  autoOpen: false,
+  serveAdminPanel: false,
+  url: 'https://dashboard.youniversityapp.ga/'
+});
---
+++ src/admin/app.js
@@ -0,0 +1,14 @@
+const config = {
+  locales: ['es'],
+  tutorials: false,
+  notifications: {
+    release: false
+  },
+};
+
+const bootstrap = (/* app */) => {};
+
+export default {
+  config,
+  bootstrap,
+};
